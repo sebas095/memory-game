20b4092b428096d4b9b3f989e7f6edef6e73c171
Sebastian Duque <sebas_tian_95@hotmail.com>
2023-08-17 23:00:48 -0500

    add deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    test: {
+      environment: 'jsdom',
+    }
   }
+
+  if (command !== 'serve') {
+    config.base = '/memory-game/'
+  }
+
+  return config
 })
